b9c7ae73ced88370eae5f8524ddcb0dc8b100fa4
Boris <transkarpation@gmail.com>
2021-04-07 12:07:34 +0300

    books pagination

--- controllers/books.controller.js
+++ controllers/books.controller.js
@@ -16,23 +16,22 @@ module.exports = {
 
     get: async (req, res) => {
         let { startAt, maxResults } = req.query
-        try {
-            // const results = await knex('books as b')
-            //     .join('users as u', 'u.id', 'b.user_id')
-            //     .select('b.id', 'u.email as creator_email', 'b.title', 'b.author')
-            // await results
-            const response = {
-                startAt: 0,
-                maxResults: 10,
-                isLast: false,
-                total: 200,
-                values: []
-            }
-            // const result = await knex.select().table('books')
-            return res.send(response)
-        } catch (e) {
-            return res.status(500).end()
+
+        startAt  = Number.isInteger(+startAt) ? Math.abs(startAt) : 0;
+        maxResults = Number.isInteger(+maxResults) ? Math.abs(maxResults) : 10;
+
+        let results = await knex('books').count('id as total')
+        const total = results[0].total
+        const values = await knex.select('*').from('books').orderBy('title').limit(maxResults).offset(startAt)
+
+        const response = {
+            startAt: startAt,
+            maxResults: maxResults,
+            total: +total,
+            values
         }
+        // const result = await knex.select().table('books')
+        return res.send(response)
         
     },
 
